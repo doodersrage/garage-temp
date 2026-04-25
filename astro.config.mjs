728d29f54cbc7b19e4fe46f1db7e2544fe9edd26
Robert McDowell <doodersrage@gmail.com>
2026-04-25 18:51:53 -0400

    weather and terrible/fun styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     host: true
   },
+
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
---
+++ src/lib/FetchWeather.ts
@@ -0,0 +1,22 @@
+import { defineMiddleware } from "astro:middleware";
+
+export async function fetchWeather(): Promise<any> {
+  const apiKey = import.meta.env.NEXT_PUBLIC_OPENWEATHER_API_KEY;
+  const city = import.meta.env.NEXT_PUBLIC_OPENWEATHER_CITY_ID;
+  const url = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}&units=imperial`;
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error('Weather data not found');
+    }
+
+    const data = await response.json();
+    return data;
+
+  } catch (e) {
+    console.error("Global error caught:", e);
+    throw e; 
+  }
+  
+}
