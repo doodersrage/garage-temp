9f1cbe83650aed9a327076d8cd5d874776efb9d1
Robert McDowell <doodersrage@gmail.com>
2026-04-29 15:00:53 -0400

    clouflare mailer binding

--- src/lib/procContact.ts
+++ src/lib/procContact.ts
@@ -7,45 +7,47 @@ export async function procContact(data: { name: string; email: string; message: 
     let successSubmission = false;
     let errorMessage = "";
 
-    try {
+    // Validation logic
+    const hasErrors = Object.values(errors).some(msg => msg)
 
-        // Validation logic
-        const hasErrors = Object.values(errors).some(msg => msg)
+    if (!hasErrors) {
 
-        if (!hasErrors) {
+        const msg = createMimeMessage();
+        msg.setSender({ name: "Sender", addr: data.email });
+        msg.setRecipient(import.meta.env.SMTP_MAIL_FROM);
+        msg.setSubject('Contact Form Submission');
+        msg.addMessage({
+        contentType: "text/plain",
+        data: 'Name: ' + data.name + '\nEmail: ' + data.email + '\nMessage: ' + data.message,
+        });
 
-            const msg = createMimeMessage();
-            msg.setSender({ name: "Sender", addr: data.email });
-            msg.setRecipient(import.meta.env.SMTP_MAIL_FROM);
-            msg.setSubject('Contact Form Submission');
-            msg.addMessage({
-            contentType: "text/plain",
-            data: 'Name: ' + data.name + '\nEmail: ' + data.email + '\nMessage: ' + data.message,
-            });
+        var message = new EmailMessage(
+        data.email,
+        import.meta.env.SMTP_MAIL_FROM,
+        msg.asRaw(),
+        );
 
-            var message = new EmailMessage(
-            data.email,
-            import.meta.env.SMTP_MAIL_FROM,
-            msg.asRaw(),
-            );
+        try {
 
-            successSubmission = true;
+            await env.MAILER.send(message);
 
             const db = await initDb();
             const qry = 'INSERT INTO contacts (name, email, message) VALUES($1, $2, $3)';
             const values = [data.name, data.email, JSON.stringify(data.message)];
             await db.query(qry, values);
 
+            successSubmission = true;
+
             return { success: successSubmission, errors, errorMessage };
+
+        } catch (e) {           
+            if (e instanceof Error) {
+                errorMessage = e.message;
+            
+                return { success: successSubmission, errors, errorMessage };
+            }
         }
 
-    } catch (error) {
-        if (error instanceof Error) {
-            console.error(error.message);
-            errorMessage = error.message;
-            
-            return { success: successSubmission, errors, errorMessage };
-        }
     }
   
 }
